bae851d5875c6f6157c2c98ae5819a74b00356e6
nboutin <boutwork@gmail.com>
2018-10-03 22:23:49 +0200

    add nim test

--- test/src/test_nim.cpp
+++ test/src/test_nim.cpp
@@ -5,7 +5,31 @@
 
 using namespace nim;
 
-TEST_CASE("default", "[nim]")
+TEST_CASE("finish", "[nim]")
 {
-	Nim n;
+    Nim n;
+    REQUIRE_FALSE(n.is_finished());
+    n.play(3);
+    REQUIRE_FALSE(n.is_finished());
+    n.play(3);
+    REQUIRE_FALSE(n.is_finished());
+    n.play(3);
+    REQUIRE_FALSE(n.is_finished());
+    n.play(3);
+    REQUIRE_FALSE(n.is_finished());
+    n.play(3);
+    REQUIRE(n.is_finished());
+}
+
+TEST_CASE("winner", "[nim]")
+{
+    Nim n;
+    REQUIRE(n.get_winner_player() == nullptr);
+    n.play(3);
+    n.play(3);
+    n.play(3);
+    n.play(3);
+    n.play(3);
+    REQUIRE(n.get_winner_player() != nullptr);
+    REQUIRE(*n.get_winner_player() == n.get_player(player_e::p2));
 }
