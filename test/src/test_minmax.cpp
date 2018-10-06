1c65381994b53f3d11c91653f8b6176ad806246e
nboutin <boutwork@gmail.com>
2018-10-06 23:13:05 +0200

    add test minmax with nim=6&7

--- test/src/test_minmax.cpp
+++ test/src/test_minmax.cpp
@@ -7,6 +7,70 @@
 using namespace nim;
 using namespace ai;
 
+TEST_CASE("nim = 7", "[minmax]")
+{
+    std::vector<Player> players{{"ai", true}, {"p2", false}};
+    Nim n(7);
+    n.set_player(player_e::p1, players.at(0));
+    n.set_player(player_e::p2, players.at(1));
+
+    SECTION("depth 1")
+    {
+        Minmax m(n.get_player(player_e::p1), 1);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 1);
+    }
+
+    SECTION("depth 2")
+    {
+        Minmax m(n.get_player(player_e::p1), 2);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 1);
+    }
+
+    SECTION("depth 3")
+    {
+        Minmax m(n.get_player(player_e::p1), 3);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 2);
+    }
+}
+
+TEST_CASE("nim = 6", "[minmax]")
+{
+    std::vector<Player> players{{"ai", true}, {"p2", false}};
+    Nim n(6);
+    n.set_player(player_e::p1, players.at(0));
+    n.set_player(player_e::p2, players.at(1));
+
+    SECTION("depth 1")
+    {
+        Minmax m(n.get_player(player_e::p1), 1);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 1);
+    }
+
+    SECTION("depth 2")
+    {
+        Minmax m(n.get_player(player_e::p1), 2);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 1);
+    }
+
+    SECTION("depth 3")
+    {
+        Minmax m(n.get_player(player_e::p1), 3);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 1);
+    }
+}
+
 TEST_CASE("nim = 5", "[minmax]")
 {
     std::vector<Player> players{{"ai", true}, {"p2", false}};
