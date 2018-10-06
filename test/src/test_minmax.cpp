f287e62510e4f37715470c4316c0ad524fdac07d
nboutin <boutwork@gmail.com>
2018-10-06 23:03:33 +0200

    add test minmax with nim=4

--- test/src/test_minmax.cpp
+++ test/src/test_minmax.cpp
@@ -7,6 +7,46 @@
 using namespace nim;
 using namespace ai;
 
+TEST_CASE("nim = 4", "[minmax]")
+{
+    std::vector<Player> players{{"ai", true}, {"p2", false}};
+    Nim n(4);
+    n.set_player(player_e::p1, players.at(0));
+    n.set_player(player_e::p2, players.at(1));
+
+    SECTION("depth 0")
+    {
+        Minmax m(n.get_player(player_e::p1), 0);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 1);
+    }
+
+    SECTION("depth 1")
+    {
+        Minmax m(n.get_player(player_e::p1), 1);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 3);
+    }
+
+    SECTION("depth 2")
+    {
+        Minmax m(n.get_player(player_e::p1), 2);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 3);
+    }
+
+    SECTION("depth 3")
+    {
+        Minmax m(n.get_player(player_e::p1), 3);
+        auto move = m.compute(n, Minmax::algo::minmax);
+
+        REQUIRE(move == 3);
+    }
+}
+
 TEST_CASE("nim = 3", "[minmax]")
 {
     std::vector<Player> players{{"ai", true}, {"p2", false}};
